bd3bfea861cf4e9b27b2855b3625b18521fa507c
nBao <tonhatbao@gmail.com>
2026-06-02 03:29:54 +0700

    fix static file

--- hotel_backend/hotel_backend/settings/base.py
+++ hotel_backend/hotel_backend/settings/base.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'corsheaders',
